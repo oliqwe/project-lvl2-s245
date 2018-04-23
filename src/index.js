75dc5e13bd79d39a83b207bfdd0f189b6e62a828
Oli <oliqwe@gmail.com>
2018-04-23 16:55:46 +0300

    flat files compare in progress

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import program from 'commander';
+import fs from 'fs';
+import path from 'path';
 
-export default args =>
-  (program
-    .version('0.0.6')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<firstConfig>  <secondConfig>')
-    .option('-f, --format [type]', 'output format')
-    .parse(args));
+const findDifferences = (before, after) =>{
+  const parsedBefore = JSON.parse(before);
+  const parsedAfter = JSON.parse(after);
+  console.log(parsedBefore, parsedAfter)
+}
 
+program
+  .version('0.0.6')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((before, after) => {
+    const first = fs.readFileSync(path.join(__dirname + "/json", before), 'utf8')
+    const second = fs.readFileSync(path.join(__dirname + "/json", after), 'utf8')
+    findDifferences(first, second)
+  })
+
+  
+
+
+
+export default args => program.parse(args)
