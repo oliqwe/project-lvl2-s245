06b7b6f2b5c5cb91e19c3685993acdeb4fcb5b8c
Oli <oliqwe@gmail.com>
2018-04-25 00:17:47 +0300

    test fix

--- __tests__/genDifference.test.js
+++ __tests__/genDifference.test.js
@@ -2,9 +2,9 @@
 import fs from 'fs';
 import genDiff from './../src';
 
-const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
 
 test('difference between 2 json files', () => {
+  const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
   const after = '__tests__/__fixtures__/before.json';
   const before = '__tests__/__fixtures__/after.json';
 
@@ -13,6 +13,7 @@ test('difference between 2 json files', () => {
 
 
 test('difference between 2 yaml files', () => {
+  const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
   const after = '__tests__/__fixtures__/before.yaml';
   const before = '__tests__/__fixtures__/after.yaml';
 
@@ -21,6 +22,7 @@ test('difference between 2 yaml files', () => {
 
 
 test('difference between 2 ini files', () => {
+  const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
   const after = '__tests__/__fixtures__/before.ini';
   const before = '__tests__/__fixtures__/after.ini';
 
