ca76badcafb0e8927dea1331c993e054c73c7e0c
Oli <oliqwe@gmail.com>
2018-04-24 23:03:25 +0300

    step 2 fix

--- __tests__/genDifference.test.js
+++ __tests__/genDifference.test.js
@@ -2,11 +2,11 @@
 import fs from 'fs';
 import genDiff from './../src';
 
-const after = '__tests__/__fixtures__/before.json';
-const before = '__tests__/__fixtures__/after.json';
-
-const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
 
 test('difference between 2 json files', () => {
+  const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
+  const after = '__tests__/__fixtures__/before.json';
+  const before = '__tests__/__fixtures__/after.json';
+
   expect(genDiff(after, before)).toBe(result);
 });
--- src/index.js
+++ src/index.js
@@ -4,25 +4,21 @@ import lodash from 'lodash';
 export default (first, second) => {
   const before = JSON.parse(fs.readFileSync(first, 'utf8'));
   const after = JSON.parse(fs.readFileSync(second, 'utf8'));
-  const mergedObj = { ...before, ...after };
 
-  const result = Object.keys(mergedObj).reduce((acc, val) => {
+  const result = Object.keys({ ...before, ...after }).reduce((acc, val) => {
     if (!lodash.has(before, val)) {
-      return [...acc, ['+', [val, after[val]]]];
+      return `${acc} + ${val}: ${after[val]}\n`;
     }
 
     if (!lodash.has(after, val)) {
-      return [...acc, ['-', [val, before[val]]]];
+      return `${acc} - ${val}: ${before[val]}\n`;
     }
 
     if (before[val] === after[val]) {
-      return [...acc, [' ', [val, before[val]]]];
+      return `${acc}   ${val}: ${before[val]}\n`;
     }
+    return `${acc} + ${val}: ${after[val]}\n - ${val}: ${before[val]}\n`;
+  }, '');
 
-    return [...acc, ['+', [val, after[val]]],
-      ['-', [val, before[val]]],
-    ];
-  }, []);
-
-  return `{\n${result.reduce((acc, val) => (`${acc} ${val[0]} ${val[1].join(': ')}\n`), '')}}`;
+  return `{\n${result}}`;
 };
