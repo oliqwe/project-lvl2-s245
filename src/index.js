0dee3cfaccdc34296f3133dffb01b4ca14ee37ce
Oli <oliqwe@gmail.com>
2018-04-25 11:34:25 +0300

    step 2 fix

--- src/index.js
+++ src/index.js
@@ -10,20 +10,20 @@ export default (first, second) => {
   const before = parse(fs.readFileSync(first, 'utf8'));
   const after = parse(fs.readFileSync(second, 'utf8'));
 
-  const result = Object.keys({ ...before, ...after }).reduce((acc, val) => {
-    if (!lodash.has(before, val)) {
-      return `${acc} + ${val}: ${after[val]}\n`;
-    }
+  const result = Object.keys({ ...before, ...after })
+    .reduce((acc, val) => {
+      if (!lodash.has(before, val)) {
+        return [...acc, ` + ${val}: ${after[val]}`];
+      }
 
-    if (!lodash.has(after, val)) {
-      return `${acc} - ${val}: ${before[val]}\n`;
-    }
+      if (!lodash.has(after, val)) {
+        return [...acc, ` - ${val}: ${before[val]}`];
+      }
 
-    if (before[val] === after[val]) {
-      return `${acc}   ${val}: ${before[val]}\n`;
-    }
-    return `${acc} + ${val}: ${after[val]}\n - ${val}: ${before[val]}\n`;
-  }, '');
-
-  return `{\n${result}}`;
+      if (before[val] === after[val]) {
+        return [...acc, `   ${val}: ${before[val]}`];
+      }
+      return [...acc, ` + ${val}: ${after[val]}`, ` - ${val}: ${before[val]}`];
+    }, []);
+  return `{\n${result.join('\n')}\n}`;
 };
