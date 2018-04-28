97c8ef888f237c781f838244bbd87eb1ec8e482c
oliqwe <oliqwe@gmail.com>
2018-04-28 19:27:38 +0300

    step 5 fix

--- src/gendiffAST.js
+++ src/gendiffAST.js
@@ -1,34 +1,47 @@
 
 import _ from 'lodash';
 
+
+const types = [
+  {
+    type: 'nested',
+    check: (before, after, val) => (_.isObject(before[val]) && _.isObject(after[val])),
+    process: (before, after, fn) => fn(before, after),
+  },
+  {
+    type: 'added',
+    check: (before, after, val) => !_.has(before, val),
+    process: (before, after) => _.identity(after),
+  },
+  {
+    type: 'removed',
+    check: (before, after, val) => !_.has(after, val),
+    process: before => _.identity(before),
+  },
+  {
+    type: 'unchanged',
+    check: (before, after, val) =>
+      _.has(before, val) && _.has(after, val) && before[val] === after[val],
+    process: before => _.identity(before),
+  },
+  {
+    type: 'changed',
+    check: (before, after, val) =>
+      _.has(before, val) && _.has(after, val) && before[val] !== after[val],
+    process: (before, after) => ({ old: before, new: after }),
+  },
+];
+
+
 export const parseAST = (before, after) => {
   const keys = _.union(_.keys(before), _.keys(after));
-
-  return keys.reduce((acc, val) => {
-    if (before[val] instanceof Object && after[val] instanceof Object) {
-      return [...acc, {
-        name: val, status: 'hasChildren', children: parseAST(before[val], after[val]),
-      }];
-    }
-
-    if (!_.has(before, val)) {
-      return [...acc, { name: val, status: 'added', value: after[val] }];
-    }
-
-    if (!_.has(after, val)) {
-      return [...acc, { name: val, status: 'removed', value: before[val] }];
-    }
-
-    if (_.has(before, val) && _.has(after, val)) {
-      if (before[val] === after[val]) {
-        return [...acc, { name: val, status: 'unchanged', value: before[val] }];
-      } else if (before[val] !== after[val]) {
-        return [...acc, { name: val, status: 'changed', value: { removed: before[val], added: after[val] } }];
-      }
-    }
-    return acc;
-  }, []);
+  return keys.map((val) => {
+    const { type, process } = _.find(types, item => item.check(before, after, val));
+    const value = process(before[val], after[val], parseAST);
+    return { name: val, type, value };
+  });
 };
+
 
 const stringify = (obj, offset) => {
   if (obj instanceof Object) {
@@ -38,30 +51,21 @@ const stringify = (obj, offset) => {
   return `${obj}`;
 };
 
-
 export const renderAST = (ast, offset = 2) => _.flatten(ast.map((node) => {
-  const {
-    name, status, value, children,
-  } = node;
-
-  if (status === 'hasChildren') {
-    return `${' '.repeat(offset)}  ${name}: {\n${renderAST(children, offset + 4)}\n${' '.repeat(offset + 2)}}`;
+  const { name, type, value } = node;
+  switch (type) {
+    case 'nested':
+      return `${' '.repeat(offset)}  ${name}: {\n${renderAST(value, offset + 4)}\n${' '.repeat(offset + 2)}}`;
+    case 'changed':
+      return [
+        `${' '.repeat(offset)}- ${name}: ${stringify(value.old, offset + 1)}`,
+        `${' '.repeat(offset)}+ ${name}: ${stringify(value.new, offset + 1)}`,
+      ];
+    case 'added':
+      return `${' '.repeat(offset)}+ ${name}: ${stringify(value, offset + 1)}`;
+    case 'removed':
+      return `${' '.repeat(offset)}- ${name}: ${stringify(value, offset + 1)}`;
+    default:
+      return `${' '.repeat(offset)}  ${name}: ${stringify(value, offset + 1)}`;
   }
-
-  if (status === 'changed') {
-    return [
-      `${' '.repeat(offset)}- ${name}: ${stringify(value.removed, offset + 1)}`,
-      `${' '.repeat(offset)}+ ${name}: ${stringify(value.added, offset + 1)}`,
-    ];
-  }
-
-  if (status === 'added') {
-    return `${' '.repeat(offset)}+ ${name}: ${stringify(value, offset + 1)}`;
-  }
-
-  if (status === 'removed') {
-    return `${' '.repeat(offset)}- ${name}: ${stringify(value, offset + 1)}`;
-  }
-
-  return `${' '.repeat(offset)}  ${name}: ${stringify(value, offset + 1)}`;
 })).join('\n');
